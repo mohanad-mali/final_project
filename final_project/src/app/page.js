ed6d76fd1582fed7661523704d10af3d4d04b33d
mohanad ali <mmama22000@gmail.com>
2026-05-05 03:15:27 +0300

    fix forbidden test

--- final_project/src/app/page.js
+++ final_project/src/app/page.js
@@ -1,19 +1,23 @@
-import Hero from "@/components/Hero";
-import Categories from "@/components/Categories";
-import Offers from "@/components/Offers";
-import FeaturedProducts from "@/components/FeaturedProducts";
-import Newsletter from "@/components/Newsletter";
-import Footer from "@/components/Footer";
+// import Hero from "@/components/Hero";
+// import Categories from "@/components/Categories";
+// import Offers from "@/components/Offers";
+// import FeaturedProducts from "@/components/FeaturedProducts";
+// import Newsletter from "@/components/Newsletter";
+// import Footer from "@/components/Footer";
+
+// export default function Home() {
+//   return (
+//     <main>
+//       <Hero />
+//       <Categories />
+//       <Offers />
+//       <FeaturedProducts />
+//       <Newsletter />
+//       <Footer />
+//     </main>
+//   );
+// }
 
 export default function Home() {
-  return (
-    <main>
-      <Hero />
-      <Categories />
-      <Offers />
-      <FeaturedProducts />
-      <Newsletter />
-      <Footer />
-    </main>
-  );
+  return <h1>Hello World</h1>
 }
